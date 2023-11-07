5302e259a0d41a5f8bfd21bd167b144bbae1896e
Didgety <jnbv00@gmail.com>
2023-11-07 16:57:33 -0500

    Added dummy monsters

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,11 @@ impl GameState for State {
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Map>();
 
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] { ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph) } // only render monsters when seen
         }
     }
 }
@@ -60,6 +62,29 @@ fn main() -> rltk::BError {
 
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
+    
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for room in map.rooms.iter().skip(1) { // skip first room to not place on player
+        let (x,y) = room.center();
+
+        let glyph : rltk::FontCharType;
+        let roll = rng.roll_dice(1, 2);
+        match roll {
+            1 => { glyph = rltk::to_cp437('g') } // select a random monster to spawn
+            _ => { glyph = rltk::to_cp437('o') }
+        }
+
+        gs.ecs.create_entity()
+            .with(Position{ x, y })
+            .with(Renderable{
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
+            .build();
+    }
+    
     gs.ecs.insert(map);
 
     gs.ecs
