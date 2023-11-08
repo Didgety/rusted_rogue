6c7e28a2bb9d2f469b0fffc271f13d34b8dc7a69
Didgety <jnbv00@gmail.com>
2023-11-07 21:24:35 -0500

    comments

--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -17,14 +17,14 @@ impl<'a> System<'a> for MonsterAI {
         let (mut map, player_pos, mut viewshed, monster, name, mut position) = data;
 
         for (mut viewshed,_monster,name,mut pos) in (&mut viewshed, &monster, &name, &mut position).join() {
-            if viewshed.visible_tiles.contains(&*player_pos) {
-                console::log(&format!("{} shouts insults", name.name));
-                let path = rltk::a_star_search(
+            if viewshed.visible_tiles.contains(&*player_pos) { // if the player is visible to a monster
+                console::log(&format!("{} shouts insults", name.name)); // yell at the player
+                let path = rltk::a_star_search( // find a path and distance to the player
                     map.xy_idx(pos.x, pos.y) as i32,
                     map.xy_idx(player_pos.x, player_pos.y) as i32,
                     &mut *map
                 );
-                if path.success && path.steps.len()>1 {
+                if path.success && path.steps.len()>1 { // move one step towards the player if at least 2 steps away
                     pos.x = path.steps[1] as i32 % map.width;
                     pos.y = path.steps[1] as i32 / map.width;
                     viewshed.dirty = true;
