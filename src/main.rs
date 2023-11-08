01f8b2eeb1eed5b20ab22813c213d526f9f30e0f
Didgety <jnbv00@gmail.com>
2023-11-07 23:27:01 -0500

    added ui with health bar

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ mod melee_combat_system;
 pub use melee_combat_system::MeleeCombatSystem;
 mod damage_system;
 pub use damage_system::DamageSystem;
+mod gui;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum RunState { AwaitingInput, PreRun, PlayerTurn, MonsterTurn }
@@ -87,6 +88,8 @@ impl GameState for State {
             let idx = map.xy_idx(pos.x, pos.y);
             if map.visible_tiles[idx] { ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph) } // only render monsters when seen
         }
+
+        gui::draw_ui(&self.ecs, ctx);
     }
 }
 
