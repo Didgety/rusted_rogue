a3b69c418ed3f94221def0a2bb42cb3abb6dd9ba
Didgety <jnbv00@gmail.com>
2023-11-09 02:24:03 -0500

    added main menu

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,15 @@ mod inventory_system;
 pub use inventory_system::{ ItemCollectionSystem, ItemDropSystem, ItemUseSystem };
 
 #[derive(PartialEq, Copy, Clone)]
-pub enum RunState { AwaitingInput, PreRun, PlayerTurn, MonsterTurn, ShowInventory, ShowDropItem,
-                    ShowTargeting { range : i32, item : Entity} }
+pub enum RunState { AwaitingInput, 
+                    PreRun, 
+                    PlayerTurn, 
+                    MonsterTurn, 
+                    ShowInventory, 
+                    ShowDropItem,
+                    ShowTargeting { range : i32, item : Entity},
+                    MainMenu { menu_selection : gui::MainMenuSelection}
+                  }
 
 pub struct State {
     pub ecs: World,
@@ -58,32 +65,37 @@ impl State {
 
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
-        ctx.cls();
-
-        draw_map(&self.ecs, ctx);
-
-        {
-            let positions = self.ecs.read_storage::<Position>();
-            let renderables = self.ecs.read_storage::<Renderable>();
-            let map = self.ecs.fetch::<Map>();
-
-            let mut data = (&positions, &renderables).join().collect::<Vec<_>>();
-            data.sort_by(|&a, &b| b.1.render_order.cmp(&a.1.render_order) );
-            for (pos, render) in data.iter() {
-                let idx = map.xy_idx(pos.x, pos.y);
-                if map.visible_tiles[idx] { ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph) }
-            }
-
-            gui::draw_ui(&self.ecs, ctx);
-        }
-
         let mut newrunstate;
         {
             let runstate = self.ecs.fetch::<RunState>();
             newrunstate = *runstate;
         }
+        
+        ctx.cls();
 
         match newrunstate {
+            RunState::MainMenu {..} => {}
+            _ => {
+                draw_map(&self.ecs, ctx);
+
+                {
+                    let positions = self.ecs.read_storage::<Position>();
+                    let renderables = self.ecs.read_storage::<Renderable>();
+                    let map = self.ecs.fetch::<Map>();
+        
+                    let mut data = (&positions, &renderables).join().collect::<Vec<_>>();
+                    data.sort_by(|&a, &b| b.1.render_order.cmp(&a.1.render_order) );
+                    for (pos, render) in data.iter() {
+                        let idx = map.xy_idx(pos.x, pos.y);
+                        if map.visible_tiles[idx] { ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph) }
+                    }
+        
+                    gui::draw_ui(&self.ecs, ctx);
+                }
+            }
+        }
+
+        match newrunstate {                        
             RunState::PreRun => {
                 self.run_systems();
                 self.ecs.maintain();
@@ -145,6 +157,19 @@ impl GameState for State {
                     }
                 }
             }
+            RunState::MainMenu{..} => {
+                let result = gui::main_menu(self, ctx);
+                match result {
+                    gui::MainMenuResult::NoSelection{ selected } => newrunstate = RunState::MainMenu{ menu_selection: selected },
+                    gui::MainMenuResult::Selected{ selected } => {
+                        match selected {
+                            gui::MainMenuSelection::NewGame => newrunstate = RunState::PreRun,
+                            gui::MainMenuSelection::LoadGame => newrunstate = RunState::PreRun,
+                            gui::MainMenuSelection::Quit => { ::std::process::exit(0); }
+                        }
+                    }
+                }
+            }
         }
 
         {
@@ -188,9 +213,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<WantsToUseItem>();
     gs.ecs.register::<WantsToDropItem>();
     gs.ecs.register::<Confusion>();
-
-    gs.ecs.insert(RunState::PreRun);
-    gs.ecs.insert(gamelog::GameLog{ entries : vec!["Welcome to Rusty Rogue".to_string()] });
+    
     gs.ecs.insert(rltk::RandomNumberGenerator::new());
 
     let map : Map = Map::new_map_rooms_and_corridors();
@@ -205,6 +228,8 @@ fn main() -> rltk::BError {
     gs.ecs.insert(player_entity);
     gs.ecs.insert(map);
     gs.ecs.insert(Point::new(player_x, player_y));
+    gs.ecs.insert(gamelog::GameLog{ entries : vec!["Welcome to Rusty Rogue".to_string()] });
+    gs.ecs.insert(RunState::MainMenu{ menu_selection: gui::MainMenuSelection::NewGame });
 
     rltk::main_loop(context, gs)
 }
