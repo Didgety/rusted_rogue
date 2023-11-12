6b8db9169686d4f6f4e899d761f9abce5943437d
Didgety <jnbv00@gmail.com>
2023-11-12 02:27:00 -0500

    bloodstains on damage

--- src/map.rs
+++ src/map.rs
@@ -2,6 +2,7 @@ use rltk::{ RGB, Rltk, RandomNumberGenerator, BaseMap, Algorithm2D, Point };
 use super::{Rect};
 use serde::{Serialize, Deserialize};
 use std::cmp::{max, min};
+use std::collections::HashSet;
 use specs::prelude::*;
 
 pub const MAPWIDTH : usize = 80;
@@ -23,6 +24,7 @@ pub struct Map {
     pub visible_tiles : Vec<bool>,
     pub blocked : Vec<bool>,
     pub depth: i32,
+    pub bloodstains : HashSet<usize>,
 
     #[serde(skip_serializing)]
     #[serde(skip_deserializing)]
@@ -74,7 +76,8 @@ impl Map {
             visible_tiles : vec![false; MAPCOUNT],
             blocked : vec![false; MAPCOUNT],
             tile_content : vec![Vec::new(); MAPCOUNT],
-            depth: new_depth
+            depth: new_depth,
+            bloodstains: HashSet::new()
         };
 
         const MAX_ROOMS : i32 = 30;
@@ -223,10 +226,10 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
     let mut x = 0;
     for (idx,tile) in map.tiles.iter().enumerate() {
         // Render a tile depending upon the tile type
-
         if map.revealed_tiles[idx] {
             let glyph;
             let mut fg;
+            let mut bg = RGB::from_f32(0., 0., 0.);
             match tile {
                 TileType::Floor => {
                     glyph = rltk::to_cp437('.');
@@ -241,8 +244,13 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
                     fg = RGB::from_f32(0., 1.0, 1.0);
                 }
             }
-            if !map.visible_tiles[idx] { fg = fg.to_greyscale() } // tiles not currently visible are greyed out
-            ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
+            if map.bloodstains.contains(&idx) { bg = RGB::from_f32(0.75, 0., 0.); }
+            if !map.visible_tiles[idx] { 
+                // tiles not currently visible are greyed out
+                fg = fg.to_greyscale();
+                bg = RGB::from_f32(0., 0., 0.);
+             } 
+            ctx.set(x, y, fg, bg, glyph);
         }
 
         // Move the coordinates
