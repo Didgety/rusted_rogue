6657716e215a9bec77baadffdeff42ff1f1b4b9f
Didgety <jnbv00@gmail.com>
2023-11-24 02:36:20 -0800

    added missing match branch to random_builder()

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -31,13 +31,14 @@ pub fn random_builder(new_depth : i32) -> Box<dyn MapBuilder> {
     //Box::new(BspInteriorBuilder::new(new_depth))
     // Cellular Automata
     Box::new(CellularAutomataBuilder::new(new_depth))
-    
+
     // Fully random generator
     // let mut rng = rltk::RandomNumberGenerator::new();
     // let builder = rng.roll_dice(1, 3);
     // match builder {
     //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
     //     2 => Box::new(BspInteriorBuilder::new(new_depth)),
+    //     3 => Box::new(CellularAutomataBuilder::new(new_depth)),
     //     _ => Box::new(SimpleMapBuilder::new(new_depth))
     // }
 }
