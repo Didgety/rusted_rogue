cddf1927b7552b1db8065b0f3d6de16b68a9efcb
Didgety <jnbv00@gmail.com>
2023-11-28 23:19:37 -0500

    added ability to read rex files as maps

--- src/rex_assets.rs
+++ src/rex_assets.rs
@@ -2,6 +2,9 @@ use rltk::{ rex::XpFile };
 
 // TODO replace with original image
 rltk::embedded_resource!(SMALL_DUNGEON, "../res/SmallDungeon_80x50.xp");
+// TODO replace with new demo maps
+rltk::embedded_resource!(WFC_DEMO_IMAGE1, "../res/wfc-demo1.xp");
+rltk::embedded_resource!(WFC_DEMO_IMAGE2, "../res/wfc-demo2.xp");
 
 pub struct RexAssets {
     pub menu : XpFile
@@ -11,6 +14,8 @@ impl RexAssets {
     #[allow(clippy::new_without_default)]
     pub fn new() -> RexAssets {
         rltk::link_resource!(SMALL_DUNGEON, "../res/SmallDungeon_80x50.xp");
+        rltk::link_resource!(WFC_DEMO_IMAGE1, "../../resources/wfc-demo1.xp");
+        rltk::link_resource!(WFC_DEMO_IMAGE2, "../../resources/wfc-demo2.xp");
 
         RexAssets{
             menu : XpFile::from_resource("../res/SmallDungeon_80x50.xp").unwrap()
